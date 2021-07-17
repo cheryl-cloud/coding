7d0b95649998fc480c616576e57ad9188005f1c4
cheryl-cloud <xinru0113@gmail.com>
2021-07-17 17:43:58 +0800

    第一次安裝react

---
+++ PREN-TODO/client/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="d-flex justify-content-center">
+      <h1>hello world</h1>
+      <input />
+      <button className="btn btn-primary">save</button>
+    </div>
+  );
+}
+
+export default App;
--- PREN-TODO/server/index.js
+++ PREN-TODO/server/index.js
@@ -1,73 +1,91 @@
-const express = require("express");  //創建express library
-const app = express();  //先全域安裝npm install nodemon -g  
+const express = require("express"); //創建express library
+const app = express(); //先全域安裝npm install nodemon -g
 const cors = require("cors");
 const pool = require("./db");
 
-//middleware中介層，是一類提供系統軟件和應用軟件之間連接、便於軟件各部件之間的溝通的軟件
-app.use(cors()); //cors跨來源資源共用(跨來源資源共用（Cross-Origin Resource Sharing (CORS)）是一種使用額外 HTTP 標頭令目前瀏覽網站的使用者代理 (en-US)取得存取其他來源（網域）伺服器特定資源權限的機制。)
+//middleware中介層，是一類提供系統軟件和應用軟件之間連接、
+//便於軟件各部件之間的溝通的軟件
+//cors跨來源資源共用 跨來源資源共用（Cross-Origin Resource Sharing (CORS)）
+//是一種使用額外 HTTP 標頭令目前瀏覽網站的使用者代理 (en-US)取得存取其他來源（網域）
+//伺服器特定資源權限的機制
+app.use(cors());
 app.use(express.json()); //req.body
 
 //ROUTES//
 
 //create a todo
 
-app.post("/todos", async (req, res) => { //async
-    try {
-        const { description } = req.body;
-        const newTodo = await pool.query(
-            "INSERT INTO todo (description) VALUES($1) RETURNING *",
-            [description]
-        );
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
 
-        res.json(newTodo.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 //get all todos
 
 app.get("/todos", async (req, res) => {
-    try {
-        const allTodos = await pool.query("SELECT * FROM todo");
-        res.json(allTodos.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //get a todo
 
 app.get("/todos/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const todo = await pool.query(
-            "SELECT * FROM todo WHERE todo_id = $1",
-            [id]);
-        res.json(todo.rows[0])
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 //update a todo
 
 app.put("/todos/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { description } = req.body;
-        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",
-            [description, id]
-        );
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
 
-        res.json("Todo was updated!");
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+    res.json("Todo was updated!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //delete a todo
 
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("Todo was deleted!");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
 app.listen(5000, () => {
-    console.log("server has started on port 5000")
+  console.log("server has started on port 5000");
 });
